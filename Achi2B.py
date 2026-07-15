45dc7dcc83dbad8659e5e2f56e08e1ec203bc970
Achi <171817924+Achi3654@users.noreply.github.com>
2026-07-15 09:56:48 +0700

    Uplord2B

---
+++ Achi2B.py
@@ -0,0 +1,9 @@
+n = input("Name:")
+a = int(input("age:"))
+h = float(input("High:"))
+p = float(input("Power:"))
+m = float(input("Money:"))
+if a >= 20 or h >= 160 or m >= 100 :
+    print(n, "Pass")
+else :
+    print(n, "Not Pass")
